593fa391d641ac24a761364f90d91f98a268ce0c
Geraldine Agudelo <geraldineperea24@gmail.com>
2024-04-24 21:56:39 -0500

    Nuevo proyecto

---
+++ practica_retroalimentacion.py
@@ -0,0 +1,40 @@
+print(int("11111111", 2))
+#print(int("11001101", 2))
+#print("I like \"Monty Python\"")
+
+#names = ["John", "Mary"]
+#found = False
+#for name in names:
+# if name.startswich("J"):
+# print("Found")
+# found = True
+# break
+#if not found:
+# print("NOT found")#
+
+
+#print("Hacer un algoritmo que imprima el valor de una variable")
+#var = 1
+#print("El valor de la variable es:", var)
+#print()
+#print("Fin del programa")
+
+#John = 4
+#Mary = 10
+#Adam = 9
+
+#print("Mary tiene:", Mary, " john contiene:" ,John, " Adam tiene: ", Adam )
+#total_manzanas = John + Mary + Adam
+#print("El total de manzanas es: ", total_manzanas)
+
+
+#numero1 = int(input("Ingrese el primer numero"))
+#numero2 = int(input("Ingrese el segundo numero"))
+#suma = numero1 + numero2
+#print("La suma de los dos numero es:", suma)
+#print("Fin del programa")
+
+#print(2**2**3)
+
+n = int(input("ingrese un numero"))
+print(n >= 100)
